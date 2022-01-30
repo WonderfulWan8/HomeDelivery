f8742d2535bd367670707fdc6875e596ff20a588
WonderfulWan8 <fudengweiw@gmail.com>
2022-01-30 19:32:52 +0800

    重新布局订单列表

--- src/views/orderConfirmation/OrderConfirmation.vue
+++ src/views/orderConfirmation/OrderConfirmation.vue
@@ -22,34 +22,36 @@
                 </div>
             </div>
         </div>
+        
         <div class="products">
             <div class="products__title">
                 {{shopName}}
             </div> 
-            <div class="products__list">
-                
-                <template
-                    v-for="item in productList"
-                    :key="item._id"
-                >
-                    <div class="products__item" v-if="item.count > 0">
-                        <img :src="item.imgUrl" class="products__item__img" alt="">
-                        <div class="products__item__detail">
-                            <h4 class="products__item__title">{{item.name}}</h4>
-                            <p class="products__item__price">
-                                <span class="products__item__single">
-                                    <span class="products__item__yen">&yen;</span> 
-                                    {{item.price}} × {{item.count}}
-                                </span>
-                                <span class="products__item__total">
-                                    <span class="products__item__yen">&yen;</span> 
-                                    {{(item.price * item.count).toFixed(2)}}
-                                </span>
-                            </p>
+            <div class="products__wrapper">
+                <div class="products__list">
+                    <template
+                        v-for="item in productList"
+                        :key="item._id"
+                    >
+                        <div class="products__item" v-if="item.count > 0">
+                            <img :src="item.imgUrl" class="products__item__img" alt="">
+                            <div class="products__item__detail">
+                                <h4 class="products__item__title">{{item.name}}</h4>
+                                <p class="products__item__price">
+                                    <span class="products__item__single">
+                                        <span class="products__item__yen">&yen;</span> 
+                                        {{item.price}} × {{item.count}}
+                                    </span>
+                                    <span class="products__item__total">
+                                        <span class="products__item__yen">&yen;</span> 
+                                        {{(item.price * item.count).toFixed(2)}}
+                                    </span>
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                </template>
-            </div>    
+                    </template>
+                </div>
+            </div>  
         </div>
         <div class="order">
             <div class="order__price">实付金额 <b>¥{{caculations.price}}</b></div>
@@ -143,62 +145,72 @@ export default{
         }
     }
     .products{
-        margin:.16rem .18rem .55rem .18rem;//.55rem给底部实付金额留出位置
+        margin:.16rem .18rem .1rem .18rem;//.55rem给底部实付金额留出位置
         background-color: #fff;
         &__title{
-           padding: 16px 0 0 16px;
-           font-size: 16px;
+           padding: .16rem;
+           font-size: .16rem;
            color: #333;
            line-height: 22px;
            font-weight: bold;
         }
+        &__list{
+            background-color: white;
+            // padding-bottom: 0.16rem;
+        }
+        &__wrapper{
+            overflow-y: scroll;
+            margin: 0 .18rem;
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 2.6rem;
+            bottom: .6rem;
+        }
         &__item{
-            overflow-y: scroll;//y轴滚动
             flex: 1;
-                position: relative;
+            position: relative;
+            display: flex;
+            padding: 0 .16rem .16rem .16rem;
+            &__detail{
+                overflow:hidden;
+            }
+            &__img{
+                width: 0.46rem;
+                height: 0.46rem;
+                margin-right: .16rem;
+            }
+            &__title{
+                line-height: .2rem;
+                font-size: .14rem;
+                color: $content-fontcolor;
+                margin: 0;
+                margin-bottom: 6px;
+                @include ellipsis
+                
+            }
+            &__price{
                 display: flex;
-                padding: .12rem 0;//下面有边框，用padding会有问题
-                margin: 0 .16rem;
-                border-bottom: .01rem solid $content-bgColor;
-                &__detail{
-                    overflow:hidden;
-                }
-                &__img{
-                    width: 0.46rem;
-                    height: 0.46rem;
-                    margin-right: .16rem;
-                }
-                &__title{
-                    line-height: .2rem;
-                    font-size: .14rem;
-                    color: $content-fontcolor;
-                    margin: 0;
-                    margin-bottom: 6px;
-                    @include ellipsis
-                    
-                }
-                &__price{
-                    display: flex;
-                    margin: 0;//重置p标签的margin
-                    line-height: .2rem;
-                    font-size: .14rem;
-                    color: $highlight-fontColor;
-                    
-                }
-                &__single{
-                    width: 160px;
-                }
-                &__total{
-                    color: #333;
-                    font-weight: bold;
-                }
-                &__yen{
-                    font-size: .12rem;
-                }
+                margin: 0;//重置p标签的margin
+                line-height: .2rem;
+                font-size: .14rem;
+                color: $highlight-fontColor;
+                
+            }
+            &__single{
+                width: 155px;
+            }
+            &__total{
+                color: #333;
+                font-weight: bold;
+            }
+            &__yen{
+                font-size: .12rem;
+            }
         }
     }
     .order{
-        position: fixed;
+        position: absolute;
         left: 0px;
         right: 0px;
         bottom: 0px;
